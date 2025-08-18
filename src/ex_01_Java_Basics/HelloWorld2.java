26563440ad89c8ed1b7e1c85a46cb21226253468
vinayrk77 <vinayrk26@gmail.com>
2025-08-18 09:38:09 +0530

    18th August

---
+++ src/ex_01_Java_Basics/HelloWorld2.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class HelloWorld2 {
+    public static void main(String[] args){
+        System.out.println("My Name is Vinay");
+    }
+}
---
+++ src/ex_01_Java_Basics/HelloWorld3.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class HelloWorld3 {
+    public static void main(String[] args) {
+        System.out.println("My name is sam");
+    }
+}
